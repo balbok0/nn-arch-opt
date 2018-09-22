7bc3b47548a8420b959629167f8eb8b9c15048db
balbok0 <jakubflpk@gmail.com>
2018-09-22 16:18:54 -0700

    fix to imports?

--- genetic/helpers/__init__.py
+++ genetic/helpers/__init__.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.append(os.path.realpath(__file__)[:-11])
 
-from helpers.helpers import *
-from helpers.helpers_data import *
-from helpers.helpers_mutate import *
+from helpers import *
+from helpers_data import *
+from helpers_mutate import *
