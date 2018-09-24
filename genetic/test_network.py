95fe468ccb843433d35eec02eb732ee10da7960f
balbok0 <jakubflpk@gmail.com>
2018-09-23 22:45:36 -0700

    switched test to support pytest

--- genetic/test_network.py
+++ genetic/test_network.py
@@ -1,11 +1,20 @@
-def check_drop_last_in_arch():
+from program_variables import program_params
+from helpers.helpers_data import prepare_data
+
+x, y = prepare_data('testing', first_time=False)
+program_params.input_shape.fset(x.shape[1:])
+program_params.output_shape.fset(len(y[0]))
+program_params.debug = True
+
+
+def test_drop_last_in_arch():
     from network import Network
 
     n2 = Network(architecture=[((3, 3), 32), 'max', ((5, 5), 6), 32, 'drop0.4'], opt='sgd', activation='relu')
     assert not (isinstance(n2.arch[-1], str) and n2.arch[-1].startswith('drop'))
 
 
-def check_add_conv_max_seq():
+def test_add_conv_max_seq():
     from helpers.helpers_mutate import add_conv_max
     from network import Network
 
@@ -13,7 +22,7 @@ def check_add_conv_max_seq():
     print(add_conv_max(n2, 3).arch)
 
 
-def check_priv_add_conv_max_seq():
+def test_priv_add_conv_max_seq():
     from helpers.helpers_mutate import __add_conv_max
     from network import Network
 
@@ -23,7 +32,7 @@ def check_priv_add_conv_max_seq():
     print(__add_conv_max(n2, 5, 3, ((3, 3), 5)).arch)
 
 
-def check_add_dense_drop_seq():
+def test_add_dense_drop_seq():
     from helpers.helpers_mutate import add_dense_drop
     from network import Network
 
@@ -33,7 +42,7 @@ def check_add_dense_drop_seq():
     print(add_dense_drop(n2).arch)
 
 
-def check_priv_add_dense_drop_seq():
+def test_priv_add_dense_drop_seq():
     from helpers.helpers_mutate import __add_dense_drop
     from network import Network
 
@@ -44,7 +53,7 @@ def check_priv_add_dense_drop_seq():
     print(__add_dense_drop(n2, 1, 11, 'drop0.9').arch)
 
 
-def check_rmv_conv_max_seq():
+def test_rmv_conv_max_seq():
     from helpers.helpers_mutate import remove_conv_max
     from network import Network
 
@@ -54,7 +63,7 @@ def check_rmv_conv_max_seq():
     print(remove_conv_max(n2).arch)
 
 
-def check_priv_rmv_conv_max_seq():
+def test_priv_rmv_conv_max_seq():
     from helpers.helpers_mutate import __remove_conv_max
     from network import Network
 
@@ -66,7 +75,7 @@ def check_priv_rmv_conv_max_seq():
     print(__remove_conv_max(n2, 4, 7).arch)
 
 
-def check_rmv_dense_drop_seq():
+def test_rmv_dense_drop_seq():
     from helpers.helpers_mutate import remove_dense_drop
     from network import Network
 
@@ -76,7 +85,7 @@ def check_rmv_dense_drop_seq():
     print(remove_dense_drop(n2).arch)
 
 
-def check_priv_dense_drop_seq():
+def test_priv_dense_drop_seq():
     from helpers.helpers_mutate import __add_dense_drop
     from network import Network
 
@@ -87,7 +96,7 @@ def check_priv_dense_drop_seq():
     print(__add_dense_drop(n2, 11, 32, 'drop0.70').arch)
 
 
-def check_mutate_parent():
+def test_mutate_parent():
     from network import Network
 
     n1 = Network(
@@ -101,7 +110,7 @@ def check_mutate_parent():
     print([i.arch for i in Network._mutate_parent(n1, n2)])
 
 
-def check_mutate_parent_2():
+def test_mutate_parent_2():
     from network import Network
 
     n1 = Network(
@@ -120,36 +129,3 @@ def check_mutate_parent_2():
          32, 'drop0.30', 128, 'drop0.30', 32]
     )
     print([i.arch for i in Network._mutate_parent_2(n1, n2)])
-
-
-def main():
-    """
-    Always calls all the functions in this file, in alphabetical order. Does not call itself.
-    """
-    from program_variables import program_params
-    from helpers.helpers_data import prepare_data
-
-    x, y = prepare_data('testing', first_time=False)
-    program_params.input_shape.fset(x.shape[1:])
-    program_params.output_shape.fset(len(y[0]))
-    program_params.debug = True
-
-    import sys
-    import inspect
-
-    current_module = sys.modules[__name__]
-    all_functions = inspect.getmembers(current_module, inspect.isfunction)
-    for key, value in all_functions:
-        try:
-            args = inspect.getfullargspec(value).args
-        except AttributeError:
-            # noinspection PyDeprecation
-            args = inspect.getargspec(value).args
-        if args == [] and not key == 'main':
-            print('NEW TEST: {}\n\n'.format(key))
-            value()
-            print('\nEND TEST: {}\n\n\n\n'.format(key))
-
-
-if __name__ == '__main__':
-    main()
