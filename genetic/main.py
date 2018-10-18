3be58b34077a2d26e3d6b19ef6818024430b56d6
balbok0 <jakubflpk@gmail.com>
2018-10-18 10:44:18 -0700

    updated main, to not use generator

--- genetic/main.py
+++ genetic/main.py
@@ -3,7 +3,8 @@ def main():
     from helpers.helpers_data import prepare_data
     m = Mutator(population_size=4, generator_f=prepare_data, generator_args=['testing', False])
     (x, y), (x_val, y_val) = prepare_data('colorflow - Hgg_vs_Hqq 50')
-    print(m.evolve(x, y, validation_data=(x_val, y_val), batch_size=1000, verbose=1, generations=20))
+    print(m.evolve(x, y, validation_data=(x_val, y_val), batch_size=1000, verbose=1, generations=20,
+                   use_generator=False))
 
 
 if __name__ == '__main__':
